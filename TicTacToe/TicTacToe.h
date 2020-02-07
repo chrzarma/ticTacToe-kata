b2202aacb99825f7a45200f02b56ec3cd3d3a3ed
Christian Zarmakoupis <chrzarma@gmail.com>
2020-02-07 16:33:24 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToe.h
@@ -0,0 +1,19 @@
+//
+//  TicTacToe.h
+//  TicTacToe
+//
+//  Created by Christian Zarmakoupis on 07/02/2020.
+//  Copyright © 2020 Chris Zarmakoupis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TicTacToe.
+FOUNDATION_EXPORT double TicTacToeVersionNumber;
+
+//! Project version string for TicTacToe.
+FOUNDATION_EXPORT const unsigned char TicTacToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TicTacToe/PublicHeader.h>
+
+
